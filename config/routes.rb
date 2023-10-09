05de05c00d24e75b9d8040e25356afcc5e11a91a
Aamir <aamirasaram2107@gmail.com>
2023-10-09 15:08:24 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "recipes#index"
+
+  resources :users, only: [:index, :show] do
+    resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+      resources :recipe_foods, only: [:index, :new, :create, :destroy]
+    end
+    resources :foods, only: [:index, :new, :create]
+  end
+
+  get "/public_recipes", to: "recipes#index", as: "public_recipes"
+  get "/recipes/:id", to: "recipes#show", as: "recipe"
+  get "/recipes", to: "users#index", as: "recipes"
+  get "/foods", to: "foods#index", as: "foods"
+  get "/general_shopping_list", to: "recipe_foods#index", as: "shopping"
+
 end
