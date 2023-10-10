7bd4d8fe5691fc5e6f0cc8e40b59abee5e548e07
Aamir <aamirasaram2107@gmail.com>
2023-10-10 14:35:11 +0200

    Fix recipes not posting to database

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,4 +13,19 @@ class RecipesController < ApplicationController
     @recipe.destroy
     redirect_to recipes_path
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    redirect_to recipes_path if @recipe.save
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
