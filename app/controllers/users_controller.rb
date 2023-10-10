c978ad2df257f8307609fcca665de834ae27b896
Aamir <aamirasaram2107@gmail.com>
2023-10-10 11:49:18 +0200

    Add description to user recipe card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    user = User.find(1) # TODO: Change this to current_user
+    user = current_user
     @recipes = user.recipes
   end
 end
